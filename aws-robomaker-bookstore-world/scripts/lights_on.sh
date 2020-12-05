c89c0d482e02d96cadc73552f1d8f1ad37a128e9
shreyashpalande <shreyashpalande60@gmail.com>
2020-12-05 23:56:41 +0100

    Library world edited with arucotag

--- aws-robomaker-bookstore-world/scripts/lights_on.sh
+++ aws-robomaker-bookstore-world/scripts/lights_on.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rostopic pub -l --once /bookstore_lights aws_robomaker_bookstore_world/bookstore_light_msgs "{light_number:[1,2,3,4,5,6,7,8,9],range: 20.0}"
+rostopic pub -l --once /bookstore_lights aws_robomaker_bookstore_world/bookstore_light_msgs "{light_number:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],range: 20.0}"
